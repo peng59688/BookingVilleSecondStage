036b72982724538b0ba0555e8a4ffb7dfb81fa42
tiantan <z5133160@gmail.com>
2021-04-25 23:59:23 +1000

    signInOut

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react'
+import React from 'react'
 import logo from '../images/logo.svg'
-import {FaAlignRight} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
 import {useContext} from 'react'
 import {RoomContext} from '../context'
